b667157c3700a3b9cd2412ec469bebd186ef543b
mujeeb1238 <mujeeb1238@gmail.com>
2015-02-03 13:49:55 +0530

    warranty date added in item details

--- obs-db/migrations/core_db/V174__insert_r_enum_values_script.sql
+++ obs-db/migrations/core_db/V174__insert_r_enum_values_script.sql
@@ -5,3 +5,20 @@ update job_parameters set param_name='system' where param_name='ProvSystem';
 SET @id=(select id from job where name='RADIUS');
 insert ignore into job_parameters values(null,@id,'Mikrotik_api','String',null,
 '{"ip":"","uname":"","pwd":""}','N',null);
+
+Drop procedure IF EXISTS addWarrantyDate;
+DELIMITER //
+create procedure addWarrantyDate() 
+Begin
+  IF NOT EXISTS (
+     SELECT * FROM information_schema.COLUMNS
+     WHERE COLUMN_NAME = 'warranty_date'
+     and TABLE_NAME = 'b_item_detail'
+     and TABLE_SCHEMA = DATABASE())THEN
+ALTER TABLE `b_item_detail` ADD COLUMN `warranty_date` DATETIME NULL DEFAULT NULL  AFTER `warranty` ;
+
+END IF;
+END //
+DELIMITER ;
+call addWarrantyDate();
+Drop procedure IF EXISTS addWarrantyDate;
