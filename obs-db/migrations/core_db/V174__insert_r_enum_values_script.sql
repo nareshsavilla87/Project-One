27ebc4bb5f002582cca21c26044d859588a45927
mujeeb1238 <mujeeb1238@gmail.com>
2015-02-06 17:25:11 +0530

    script added for device swap enum value in r_enum_value table

--- obs-db/migrations/core_db/V174__insert_r_enum_values_script.sql
+++ obs-db/migrations/core_db/V174__insert_r_enum_values_script.sql
@@ -1,5 +1,6 @@
 insert ignore into r_enum_value VALUES ('radius',1,'version-1','version-1');
 insert ignore into r_enum_value VALUES ('radius',2,'version-2','version-2');
+insert ignore into r_enum_value VALUES ('device_swap',1,'Replacement','Replacement');
 
 SET SQL_SAFE_UPDATES = 0;
 update job_parameters set param_name='system' where param_name='ProvSystem';
