9e85672c98329b1fca01b988c9e0844765d1f5a2
kiran <kiran@hugotechnologies.com>
2015-04-09 15:47:49 +0530

    .

--- obsplatform/src/main/java/org/mifosplatform/portfolio/client/data/ClientData.java
+++ obsplatform/src/main/java/org/mifosplatform/portfolio/client/data/ClientData.java
@@ -112,7 +112,7 @@ final public class ClientData implements Comparable<ClientData> {
                 clientData.externalId, clientData.activationDate, clientData.imageKey, allowedOffices, clientData.groups,
                 categoryDatas,clientData.categoryType,clientData.email,clientData.phone,clientData.homePhoneNumber,clientData.addressNo,clientData.street,
                 clientData.city,clientData.state,clientData.country,clientData.zip,clientData.balanceAmount,allocationDetailsDatas,clientData.hwSerialNumber,
-                clientData.currency, groupDatas,null,balanceCheck,clientData.taxExemption,clientData.entryType,clientData.walletAmount,null,null,null,null);
+                clientData.currency, groupDatas,null,balanceCheck,clientData.taxExemption,clientData.entryType,clientData.walletAmount,null,null,null,clientData.title);
     }
 
     public static ClientData setParentGroups(final ClientData clientData, final Collection<GroupGeneralData> parentGroups) {
