cb3d2b64c64d353997a560d49d1d2fd4048eca39
ranjith15 <ranjithworld150@gmail.com>
2015-04-21 11:17:44 +0530

    ..

--- obsplatform/src/main/java/org/mifosplatform/billing/servicetransfer/api/ServiceTransferApiResource.java
+++ obsplatform/src/main/java/org/mifosplatform/billing/servicetransfer/api/ServiceTransferApiResource.java
@@ -2,7 +2,6 @@ package org.mifosplatform.billing.servicetransfer.api;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.ws.rs.Consumes;
@@ -27,7 +26,6 @@ import org.mifosplatform.infrastructure.core.serialization.ApiRequestJsonSeriali
 import org.mifosplatform.infrastructure.core.serialization.DefaultToApiJsonSerializer;
 import org.mifosplatform.infrastructure.security.service.PlatformSecurityContext;
 import org.mifosplatform.organisation.feemaster.data.FeeMasterData;
-import org.mifosplatform.portfolio.property.data.PropertyDefinationData;
 import org.mifosplatform.portfolio.property.service.PropertyReadPlatformService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
