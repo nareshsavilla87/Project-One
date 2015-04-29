f834f9f944075b53d5c68430cdea15ebe33fd4b0
kiran <kiran@hugotechnologies.com>
2015-04-29 13:33:18 +0530

    .

--- obs-db/migrations/core_db/V157__addEvent_action_table.sql
+++ obs-db/migrations/core_db/V157__addEvent_action_table.sql
@@ -10,18 +10,18 @@
   UNIQUE KEY `provisining_key` (`provision_type`)
 );
 
-insert into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (1,'Create Client','CLIENT ACTIVATION','CMS','N','N');
-insert into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (2,'Close Client','TERMINATE','CMS','N','N');
-insert into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (3,'Event Order','PROVISION IT','Radius','N','N');
-insert into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (4,'Release Device','RELEASE DEVICE','Radius','N','N');
-insert into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (5,'Create Agent','CREATE AGENT','CMS','N','N');
-insert into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (6,'Create Nas','CREATE NAS','Radius','N','N');
-insert into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (7,'Create RadSevice','CREATE RADSERVICE','Radius','N','N');
-insert into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (9,'Change Credentials','CHANGE CREDENTIALS','Radius','N','N');
-insert into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (10,'Update RadService','UPDATE RADSERVICE','Radius','N','N');
-insert into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (11,'Remove RadService','REMOVE RADSERVICE','Radius','N','N');
-insert into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (12,'Update RadSevice','UPDATE RADSERVICE','Radius','N','N');
-insert into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (13,'Remove RadSevice','REMOVE RADSERVICE','Radius','N','N');
+insert ignore into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (1,'Create Client','CLIENT ACTIVATION','CMS','N','N');
+insert ignore into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (2,'Close Client','TERMINATE','CMS','N','N');
+insert ignore into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (3,'Event Order','PROVISION IT','Radius','N','N');
+insert ignore into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (4,'Release Device','RELEASE DEVICE','Radius','N','N');
+insert ignore into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (5,'Create Agent','CREATE AGENT','CMS','N','N');
+insert ignore into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (6,'Create Nas','CREATE NAS','Radius','N','N');
+insert ignore into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (7,'Create RadSevice','CREATE RADSERVICE','Radius','N','N');
+insert ignore into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (9,'Change Credentials','CHANGE CREDENTIALS','Radius','N','N');
+insert ignore into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (10,'Update RadService','UPDATE RADSERVICE','Radius','N','N');
+insert ignore into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (11,'Remove RadService','REMOVE RADSERVICE','Radius','N','N');
+insert ignore into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (12,'Update RadSevice','UPDATE RADSERVICE','Radius','N','N');
+insert ignore into `b_provisioning_actions`(`id`,`provision_type`,`action`,`provisioning_system`,`is_enable`,`is_delete`) values (13,'Remove RadSevice','REMOVE RADSERVICE','Radius','N','N');
 
 Drop procedure IF EXISTS makercheker; 
 DELIMITER //
