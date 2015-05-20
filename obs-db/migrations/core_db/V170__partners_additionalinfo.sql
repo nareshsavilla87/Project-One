706db7c056440b27080392585b16ec4b51744315
kiran <kiran@hugotechnologies.com>
2015-05-20 15:48:19 +0530

    .

--- obs-db/migrations/core_db/V170__partners_additionalinfo.sql
+++ obs-db/migrations/core_db/V170__partners_additionalinfo.sql
@@ -1,5 +1,7 @@
--- INSERT IGNORE INTO c_paymentgateway_conf VALUES(null,'neteller',1,'');
 
+-- INSERT IGNORE INTO c_paymentgateway_conf VALUES(null,'neteller',1,'');
+SET SQL_SAFE_UPDATES = 0;
+SET foreign_key_checks = 0;
 insert ignore into m_code VALUES (null,'Office Type',0,'Office are created and mapped to type');
  
  set @id=(select id from m_code where code_name='Office Type');
@@ -250,9 +252,6 @@ CREATE TABLE IF NOT EXISTS `m_office_balance` (
   `lastmodifiedby_id` int(5) DEFAULT NULL,
   PRIMARY KEY (`id`)
   ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-
-SET SQL_SAFE_UPDATES=0;
-SET FOREIGN_KEY_CHECKS=0;
 
 Drop procedure if exists officeAddress;
 DELIMITER //
