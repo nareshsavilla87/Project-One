381975188d4467febadfe3ed8b7e692d75af5e92
kiran <kiran@hugotechnologies.com>
2015-05-25 15:31:21 +0530

    .

--- obs-db/migrations/core_db/V186.3__report_collection_day_wise_summery_chart.sql
+++ obs-db/migrations/core_db/V186.3__report_collection_day_wise_summery_chart.sql
@@ -46,6 +46,9 @@ group by pay.payment_date,pay.paymode_id
 order by off.office_type , pay.payment_date' where report_name='Collection_Day_wise_Details';
 insert ignore into stretchy_report_parameter(report_id,parameter_id,report_parameter_name)values (@CID,@offId,'Office');
 insert ignore into stretchy_report_parameter(report_id,parameter_id,report_parameter_name)values (@CID,@modId,'Payment Mode');
+
+ Insert ignore into m_role_permission 
+Select (Select id from m_role where name='selfcare') as rid,b.id from m_permission b where b.code = 'READ_CHARGECODE';
  
 
 
