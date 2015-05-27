49443dfdd8fb063bb4a0be868117fe0114732df2
kiran <kiran@hugotechnologies.com>
2015-05-27 17:20:08 +0530

    .

--- obsplatform/src/main/java/org/mifosplatform/portfolio/order/service/OrderReadPlatformServiceImpl.java
+++ obsplatform/src/main/java/org/mifosplatform/portfolio/order/service/OrderReadPlatformServiceImpl.java
@@ -149,7 +149,7 @@ public class OrderReadPlatformServiceImpl implements OrderReadPlatformService
 	public List<PaytermData> getChargeCodes(Long planCode,Long clientId) {
 
 		   context.authenticatedUser();
-	        String sql = " SELECT DISTINCT b.billfrequency_code AS billfrequency_code,b.id AS id,c.contract_period AS duration,pm.is_prepaid AS isPrepaid,a.price as price" +
+	        String sql = " SELECT DISTINCT b.billfrequency_code AS billfrequencyCode,b.id AS id,c.contract_period AS duration,pm.is_prepaid AS isPrepaid,a.price as price" +
 	        		" FROM b_charge_codes b, b_plan_master pm,b_plan_pricing a LEFT JOIN b_contract_period c ON c.contract_period = a.duration" +
 	        		"  WHERE  a.charge_code = b.charge_code AND a.is_deleted = 'n' AND a.plan_id = ? AND pm.id = a.plan_id";
 	        
