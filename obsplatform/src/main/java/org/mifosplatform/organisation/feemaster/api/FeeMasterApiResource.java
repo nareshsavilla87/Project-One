30453b40a005bfe61e9ec68aa4eedec9f422794f
kiran <kiran@hugotechnologies.com>
2015-06-11 12:06:22 +0530

    .

--- obsplatform/src/main/java/org/mifosplatform/organisation/feemaster/api/FeeMasterApiResource.java
+++ obsplatform/src/main/java/org/mifosplatform/organisation/feemaster/api/FeeMasterApiResource.java
@@ -14,6 +14,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
@@ -79,9 +80,9 @@ public class FeeMasterApiResource {
 	@GET
 	@Consumes({MediaType.APPLICATION_JSON})
 	@Produces({MediaType.APPLICATION_JSON})
-	public String retrieveAllContracts(@Context final UriInfo uriInfo) {
+	public String retrieveAllContracts(@Context final UriInfo uriInfo,@QueryParam("transactionType") String transType) {
 		context.authenticatedUser().validateHasReadPermission(resourceNameForPermissions);
-		final Collection<FeeMasterData> feeMasterData=this.feeMasterReadplatformService.retrieveAllData();
+		final Collection<FeeMasterData> feeMasterData=this.feeMasterReadplatformService.retrieveAllData(transType);
 		final ApiRequestJsonSerializationSettings settings = apiRequestParameterHelper.process(uriInfo.getQueryParameters());
 		return this.toApiJsonSerializer.serialize(settings, feeMasterData, RESPONSE_DATA_PARAMETERS);
 	}
