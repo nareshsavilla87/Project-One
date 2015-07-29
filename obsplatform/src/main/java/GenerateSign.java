0948863e3868d696e0fa8db11016e43d434bb786
kiran <kiran@hugotechnologies.com>
2015-07-29 11:31:36 +0530

    .

--- obsplatform/src/main/java/GenerateSign.java
+++ obsplatform/src/main/java/GenerateSign.java
@@ -1,18 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 public class GenerateSign {
 	
-	static char hexDigit[] = {'0', '1', '2', '3', '4', '5', '6', '7',
-'8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-
-	
-	public static void main(String []arg) {
-		
-	String str="select a.id as clientId from m_client a, b_orders b where a.id=b.client_id and b.order_status  = 1 and (b.next_billable_day is null or b.next_billable_day<=Now())";
-	if(str.contains("now()")){
-		str=str.replace("now()", "hi");
-	}
-	System.out.println(str);
+	public static void main(String []args){
+		final List<String> list = new ArrayList<String>();
+		 //list = new ArrayList<String>();
+		list.add("hi");
+		list.add("Bye");
+	    for(String string:list){
+		System.out.println(string);
+	    
+	    }
+	    list.remove(0);
+	    for(String string:list){
+			System.out.println(string);
+		    
+		    }
 	}
 	
 }
