b4aee945a3af3f724b2ddab226ccaf3dff792bda
mujeeb1238 <mujeeb1238@gmail.com>
2015-09-03 17:39:31 +0530

    script file modifications

--- obs-db/migrations/core_db/V196.6__dashboard_query_for_calendar.sql
+++ obs-db/migrations/core_db/V196.6__dashboard_query_for_calendar.sql
@@ -1,12 +1,12 @@
 INSERT IGNORE INTO `stretchy_report` (`report_name`,`report_type`,`report_subtype`,`report_category`,`report_sql`,`description`,`core_report`,`use_report`) 
 VALUES ('NetActiveCalendar','Table','','Client',
-"select actual_date, 
-concat(if ( Transaction_type ='RENEWAL BEFORE AUTOEXIPIRY','REN BEF AUTO EXP',
-if ( Transaction_type ='RENEWAL AFTER AUTOEXIPIRY','REN AFT AUTO EXP' ,Transaction_type )), 
-' = ', Count(order_id) ) cnt 
+'select actual_date, 
+concat(if ( Transaction_type =''RENEWAL BEFORE AUTOEXIPIRY'',''REN BEF AUTO EXP'',
+if ( Transaction_type =''RENEWAL AFTER AUTOEXIPIRY'',''REN AFT AUTO EXP'' ,Transaction_type )), 
+'' = '', Count(order_id) ) cnt 
 from net_activedtls_vw 
 group by actual_date, transaction_type
-",
+',
 'NetActiveCalendar',1,1);
 
 
