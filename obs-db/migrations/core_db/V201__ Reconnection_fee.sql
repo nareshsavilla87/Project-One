ee4b630b720959463651bd63c1f8c0106059ecbd
rakesh <rakesh.adlagatta@gmail.com>
2015-10-12 18:45:28 +0530

    script added for reconnection_fee

---
+++ obs-db/migrations/core_db/V201__ Reconnection_fee.sql
@@ -0,0 +1,2 @@
+insert ignore into `b_eventaction_mapping`(`id`,`event_name`,`action_name`,`process`,`is_deleted`,`is_synchronous`) 
+values (null,'Order reconnection','Reconnection_Fee','workflow_events','Y','N');
