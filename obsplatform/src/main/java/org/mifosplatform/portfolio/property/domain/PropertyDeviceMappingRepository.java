3882daf9ff4781203eb8cd4cea3915dc40209687
sanjay528 <sanjeev.2826@gmail.com>
2015-10-14 19:40:57 +0530

    schedule order property

--- obsplatform/src/main/java/org/mifosplatform/portfolio/property/domain/PropertyDeviceMappingRepository.java
+++ obsplatform/src/main/java/org/mifosplatform/portfolio/property/domain/PropertyDeviceMappingRepository.java
@@ -16,6 +16,9 @@ JpaSpecificationExecutor<PropertyDeviceMapping>{
 	@Query("from PropertyDeviceMapping propertyDeviceMapping where propertyDeviceMapping.propertyCode =:propertyCode and propertyDeviceMapping.isDeleted = 'N'")
 	List<PropertyDeviceMapping> findByPropertyCode(@Param("propertyCode")String propertyCode);
 
+	@Query("from PropertyDeviceMapping propertyDeviceMapping where propertyDeviceMapping.serialNumber =:serialNumber and propertyDeviceMapping.clientId =:clientId and propertyDeviceMapping.isDeleted = 'N'")
+	PropertyDeviceMapping findByCustomerSerailNumber(@Param("serialNumber")String serialNumber, @Param("clientId")Long clientId);
+
 
 }
 
